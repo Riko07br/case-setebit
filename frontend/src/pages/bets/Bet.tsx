f53ae318423fb6400c8783b9d63855f9cba9cf13
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-15 23:17:41 -0300

    feat(bet): basic page template

--- frontend/src/pages/bets/Bet.tsx
+++ frontend/src/pages/bets/Bet.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import BetCard from "./components/BetCard";
 
 export function Bet() {
-    return <h1>Bolões</h1>;
+    return (
+        <>
+            <h1>Bolões</h1>
+            <div>
+                <p>Salvar palpites</p>
+                <BetCard />
+            </div>
+        </>
+    );
 }
---
+++ frontend/src/pages/bets/components/BetCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Cria card para paltite do bolão
+const BetCard = () => {
+    return (
+        <ul>
+            <div>
+                <p>dia e horario</p>
+                Time 1 <input type="text" /> x <input type="text" /> Time 2
+            </div>
+            <hr />
+        </ul>
+    );
+};
+
+export default BetCard;
