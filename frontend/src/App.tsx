937046f039ce4350f2d204c09c496f7a3bde9ad8
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-17 09:59:44 -0300

    feat: check auth on load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home } from "./pages";
+import { useEffect, useState } from "react";
+import {
+    Navigate,
+    Route,
+    BrowserRouter as Router,
+    Routes,
+} from "react-router-dom";
+import { Home, NoPage } from "./pages";
 import { Login, Register } from "./pages/auth";
 import { Navbar } from "./components";
 import { Bet } from "./pages/bets";
@@ -8,20 +13,52 @@ import { Competitions } from "./pages/competitions";
 import axios from "axios";
 
 function App() {
-    let [authenticated, setAuthenticated] = useState<boolean>(false);
+    let [auth, setAuth] = useState<boolean>(false);
 
+    // Default axios config
     axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+    axios.defaults.headers.common.Accept = "*/*";
     axios.defaults.withCredentials = true;
+
+    // Initialize
+    useEffect(() => {
+        axios
+            .get("/auth/status")
+            .then((response) => {
+                console.log("sign status: " + response.data);
+                setAuth(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+                setAuth(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        // if else quando esta carregando
+    }, [auth]);
 
     return (
         <Router>
-            <Navbar />
+            <Navbar isAuth={auth} />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/bets" element={<Bet />} />
                 <Route path="/competitions" element={<Competitions />} />
+
+                <Route
+                    path="login"
+                    element={!auth ? <Login /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="register"
+                    element={!auth ? <Register /> : <Navigate to="/" />}
+                />
+
+                <Route
+                    path="bets"
+                    element={auth ? <Bet /> : <Navigate to="/login" />}
+                />
+                <Route path="*" element={<NoPage />} />
             </Routes>
         </Router>
     );
